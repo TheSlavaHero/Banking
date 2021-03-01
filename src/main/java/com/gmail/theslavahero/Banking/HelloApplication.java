3583804ae3aec7973c084239e0b65a3fba332a3f
TheSlavaHero <theslavahero@gmail.com>
2021-03-01 19:41:52 +0100

    backend initialization

---
+++ src/main/java/com/gmail/theslavahero/Banking/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.gmail.theslavahero.Banking;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
